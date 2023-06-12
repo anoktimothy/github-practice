0be1cb17b753da56d44d437eca6f137dedad1093
Anok Timothy <anoktimothy28@gmail.com>
2023-06-12 10:46:35 -0500

    practice, added today's date

--- github practice/ContentView.swift
+++ github practice/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Text("Today is June 12, 2023")
         }
         .padding()
     }
